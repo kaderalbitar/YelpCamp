8e88932f67bdbb534fecf2ffe22042d5ddef4c10
Abdul Kader <abdulkader.albitar@gmail.com>
2022-04-30 16:29:35 +0300

    Added 404 handler route. Edited the generic error handler to include statusCode and message. Added form validation basic error handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -39,9 +40,10 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+  if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
@@ -66,8 +68,13 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send('Ob boy, something went wrong!');
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
