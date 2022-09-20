41cf17e15ac3885dcbef359aaac1638fe2c6af90
Abdul Kader <abdulkader.albitar@gmail.com>
2022-09-21 01:05:39 +0300

    Installed cloudinary package. Installed multer-storage-cloudinary package. Configured cloudinary with account info. Configured CloudinaryStorage. Finalized uploading to Cloudinary.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
@@ -22,7 +23,6 @@ router.route('/:id')
   .get(catchAsync(campgrounds.showCampground))
   .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
   .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
