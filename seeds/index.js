d2486014c7110e634d58ef13a3dcafc97d0b797e
Abdul Kader <abdulkader.albitar@gmail.com>
2022-09-23 14:46:36 +0300

    Added upload to edit page.

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,18 @@ const seedDB = async () => {
       author: '6290e0ba8f7a949845d389ac',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro itaque pariatur exercitationem at quisquam odio quas amet suscipit, facilis vel illum sunt mollitia delectus, temporibus ex officia. Hic, molestiae alias?',
-      price
+      price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dxvxciwmi/image/upload/v1663779704/YelpCamp/jaxagyijoeqd87fygapw.jpg',
+          filename: 'YelpCamp/jaxagyijoeqd87fygapw',
+        },
+        {
+          url: 'https://res.cloudinary.com/dxvxciwmi/image/upload/v1663779707/YelpCamp/s9pmgcvw7dxxdmi3mccq.jpg',
+          filename: 'YelpCamp/s9pmgcvw7dxxdmi3mccq',
+        }
+      ]
     });
     await camp.save();
   }
