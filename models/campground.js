fd8a967fc62f323f0e93e69e1f1c926efd548d0b
Abdul Kader <abdulkader.albitar@gmail.com>
2022-09-23 17:29:33 +0300

    Added a thumbnail virtual property

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,21 @@ const Review = require('./review');
 const { campgroundSchema } = require('../schemas');
 const Schema = mongoose.Schema;
 
+
+//https://res.cloudinary.com/dxvxciwmi/image/upload/w_300/v1663940419/YelpCamp/iplqpyfkybdzh4niebnl.jpg
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String
-    }
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
