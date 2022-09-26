7c9d83126bd87c5bcda0d97ffcf5b111a30bd8df
Abdul Kader <abdulkader.albitar@gmail.com>
2022-09-26 22:25:02 +0300

    Handled Mongo injection

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const LocalStrategy = require('passport-local');
 
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const ExpressError = require('./utils/ExpressError');
 
 const userRoutes = require('./routes/users');
@@ -41,6 +43,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_',
+  }));
 
 const sessionConfig = {
   secret: 'thisshouldbeabettersecret!',
@@ -67,7 +72,7 @@ app.use((req, res, next) => {
   if (!['/login', '/'].includes(req.originalUrl)) {
     req.session.returnTo = req.originalUrl;
   }
-  console.log(req.session);
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
