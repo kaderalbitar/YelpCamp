1f6c243c5dcecabce3b906e126feb267a41724e5
Abdul Kader <abdulkader.albitar@gmail.com>
2022-10-01 15:03:41 +0300

    f

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,6 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-const MongoStore = require('connect-mongo');
-
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl)
 .then(() => {
