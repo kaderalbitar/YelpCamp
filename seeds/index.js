0d191b5f11a64a3432c925083788a0027925e6d9
Abdul Kader <abdulkader.albitar@gmail.com>
2022-10-01 14:53:40 +0300

    Edited seeds file to seed the online database

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,10 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const MongoStore = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl)
 .then(() => {
   console.log('Database connected');
 })
